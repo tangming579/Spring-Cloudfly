3edb680a360ed1e2716f317d089d0758ace0fdb8
Tangming <infly123@sina.com>
2019-05-03 12:18:07 +0800

    update

--- src/eureka-server/src/main/java/com/infly/eurekaserver/EurekaServerApplication.java
+++ src/eureka-server/src/main/java/com/infly/eurekaserver/EurekaServerApplication.java
@@ -3,6 +3,7 @@ package com.infly.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class EurekaServerApplication {
 
