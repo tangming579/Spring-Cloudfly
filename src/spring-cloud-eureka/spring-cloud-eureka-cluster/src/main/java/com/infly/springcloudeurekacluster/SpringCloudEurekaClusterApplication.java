c70ab29c5de5a1b3d34175188bee3b5ab3a8e3b8
Tangming <infly123@sina.com>
2019-05-07 18:34:19 +0800

    eureka cluster

---
+++ src/spring-cloud-eureka/spring-cloud-eureka-cluster/src/main/java/com/infly/springcloudeurekacluster/SpringCloudEurekaClusterApplication.java
@@ -0,0 +1,13 @@
+package com.infly.springcloudeurekacluster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEurekaClusterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEurekaClusterApplication.class, args);
+	}
+
+}
