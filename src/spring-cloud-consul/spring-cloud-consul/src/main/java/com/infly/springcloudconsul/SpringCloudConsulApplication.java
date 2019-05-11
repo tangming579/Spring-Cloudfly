e6f453ecf83a651de55d6dd258845017c05ad0de
Tangming <infly123@sina.com>
2019-05-11 11:13:42 +0800

    Consul

--- src/spring-cloud-consul/spring-cloud-consul/src/main/java/com/infly/springcloudconsul/SpringCloudConsulApplication.java
+++ src/spring-cloud-consul/spring-cloud-consul/src/main/java/com/infly/springcloudconsul/SpringCloudConsulApplication.java
@@ -2,8 +2,12 @@ package com.infly.springcloudconsul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@RestController
 public class SpringCloudConsulApplication {
 
 	public static void main(String[] args) {
