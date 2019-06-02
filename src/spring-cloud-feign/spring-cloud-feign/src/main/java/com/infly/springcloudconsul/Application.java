4d99f1502e6089f67e47b60f36e4b89dbcb262b7
Tangming <infly123@sina.com>
2019-06-02 10:15:05 +0800

    Feign

---
+++ src/spring-cloud-feign/spring-cloud-feign/src/main/java/com/infly/springcloudconsul/Application.java
@@ -0,0 +1,25 @@
+package com.infly.springcloudconsul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableDiscoveryClient
+@EnableFeignClients
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@RequestMapping("/hello")
+	public String Hello(){
+		return "Hello";
+	}
+}
