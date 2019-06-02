89c690fc7ddf29328e241ebe350d2cd0d3f6a9ff
Tangming <infly123@sina.com>
2019-06-02 11:28:33 +0800

    feign

--- src/spring-cloud-feign/spring-cloud-feign/src/main/java/com/infly/springcloudconsul/controller/ConsumerController.java
+++ src/spring-cloud-feign/spring-cloud-feign/src/main/java/com/infly/springcloudconsul/controller/ConsumerController.java
@@ -1,6 +1,8 @@
 package com.infly.springcloudconsul.controller;
 
 import com.infly.springcloudconsul.Service.HelloService;
+import com.infly.springcloudconsul.Service.UserService;
+import com.infly.springcloudconsul.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
@@ -11,34 +13,20 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-@FeignClient(name= "spring-cloud-producer",fallback = HelloService.class)
 public class ConsumerController {
 
     @Autowired
-    private LoadBalancerClient loadBalancer;
+    private HelloService helloService;
     @Autowired
-    private DiscoveryClient discoveryClient;
+    private UserService userService;
 
-    //获取所有服务
-    @RequestMapping("/services")
-    public Object services() {
-        return discoveryClient.getInstances("service-producer");
+    @RequestMapping("/test1")
+    public String Test1(){
+        return helloService.hello();
     }
 
-    //从所有服务中选择一个服务（轮询）
-    @RequestMapping("/discover")
-    public Object discover() {
-        return loadBalancer.choose("service-producer").getUri().toString();
-    }
-
-    @RequestMapping("call")
-    public String call(){
-        ServiceInstance serviceInstance = loadBalancer.choose("service-producer");
-        System.out.println("服务地址："+serviceInstance.getUri());
-        System.out.println("服务名称："+serviceInstance.getServiceId());
-
-        String callServiceResult = new RestTemplate().getForObject(serviceInstance.getUri().toString() + "hello",String.class);
-        System.out.println(callServiceResult);
-        return callServiceResult;
+    @RequestMapping("/test2")
+    public User Test2(Long userId){
+        return userService.findUserById(userId);
     }
 }
